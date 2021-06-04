e52a4ad9a8f60c0f375fde701dc68f809edd7347
Michael Nix <mpnix@protonmail.com>
2021-06-04 23:15:55 +0100

    Updated database connection credentials

--- app/config.php
+++ app/config.php
@@ -1,7 +1,11 @@
 <?php
 
-define('DBHOST', 'localhost');
-define('DBUSER', 'root');
-define('DBPASS', 'C)dfishj)3');
+// define('DBHOST', 'localhost');
+// define('DBUSER', 'root');
+// define('DBPASS', 'C)dfishj)3');
 define('DBNAME', 'gp_appt');
 define('CHARSET', 'utf8mb4');
+
+define('DBHOST', 'us-cdbr-east-04.cleardb.com');
+define('DBUSER', 'b7fe4d33393ce4');
+define('DBPASS', '918aad18');
