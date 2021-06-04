c5ca1333652920a908f484cc7f02195d91c8e9b1
Michael Nix <mpnix@protonmail.com>
2021-06-04 23:39:50 +0100

    Moved constants and Database class back to separate file

--- app/config.php
+++ app/config.php
@@ -1,9 +1,13 @@
 <?php
 
+// define('DBNAME', 'gp_appt');
+// define('CHARSET', 'utf8mb4');
+
 // define('DBHOST', 'localhost');
 // define('DBUSER', 'root');
 // define('DBPASS', 'C)dfishj)3');
-define('DBNAME', 'gp_appt');
+
+define('DBNAME', 'heroku_0c5f40e63f30201');
 define('CHARSET', 'utf8mb4');
 
 define('DBHOST', 'us-cdbr-east-04.cleardb.com');
