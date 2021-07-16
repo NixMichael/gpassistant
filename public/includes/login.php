7b4475012d0cab9e888a2b26a4451d8cc2a795dc
Michael Nix <mpnix@protonmail.com>
2021-07-16 09:11:04 +0100

    Added default login credentials

--- public/includes/login.php
+++ public/includes/login.php
@@ -3,9 +3,9 @@
     <div class="log-in">
         <h3>Log In</h3>
         <form action="includes/login.inc.php" method="POST">
-            <input type="text" name="email" placeholder="email" value="<?php echo isset($_SESSION['useremail']) ?>" autocomplete="off"/>
+            <input type="text" name="email" placeholder="email" value="admin@gp.uk" autocomplete="off"/>
             <span class="field-error"><?php if (isset($_GET['error']) && $_GET['error'] == 'emptyemail') { echo 'email field required'; } ?></span>
-            <input type="password" name="password" placeholder="password" autocomplete="off"/>
+            <input type="password" name="password" placeholder="password" value="password" autocomplete="off"/>
             <span class="field-error"><?php 
                 if (isset($_GET['error']) && $_GET['error'] == 'emptypassword') {
                     echo 'Password field required';
